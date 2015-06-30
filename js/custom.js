b69e1944c92a4213b6dba49a3e3bc71303c1e6c4
sreeyashu <sreeyashu@gmail.com>
2015-06-30 15:08:27 +0200

    JUN 30 15:08

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,8 @@
 $( document ).ready(function() {
-	var offCanvas = $('.wcm-header .header').html();
-	$('.mm_offcanvas').html(offCanvas);
+	
+	$(".yash").click(function() {
+		$(this).parent().addClass('mm_opened');
+	});
 	
     $("#inputSearchTxt").focus(function() {
   		$(this).parent().addClass('focus');
@@ -8,13 +10,21 @@ $( document ).ready(function() {
 	$("#inputSearchTxt").blur(function() {
 		setTimeout(function(){ $("#inputSearchTxt").parent().removeClass('focus') }, 90);
 	});
+	//var offCanvas = $('.wcm-header .header').html();
+	//$('.mm_offcanvas').html(offCanvas);
+	
 	
 	$(".magnify_icon").click(function() {
 		$("#inputSearchTxt").focus(function() {
 			$(this).parent().addClass('focus');
 		});
 	});
-	$('.dropdown-menu').click(function(event){
+	//$('html').click(function(event){
+//		if($(this).hasClass('mm_view')){
+//		 event.stopPropagation();
+//		}
+//	 });
+	 $('.dropdown-menu').click(function(event){
 		if($(this).hasClass('mm_view')){
 		 event.stopPropagation();
 		}
@@ -23,5 +33,6 @@ $( document ).ready(function() {
 	 $(".mm_panel .overlay_close").click(function() {
 		 $('[data-toggle="dropdown"]').parent().removeClass('open');
 	});
+	
 	
 });
